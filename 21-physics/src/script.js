ffa05a5c54fb16434543526cb4ed853b2a2b09a5
Jay Kim <mearak43@gmail.com>
2023-08-01 21:18:46 +0900

    complete 21 physics

--- 21-physics/src/script.js
+++ 21-physics/src/script.js
@@ -9,6 +9,43 @@ THREE.ColorManagement.enabled = false;
  * Debug
  */
 const gui = new dat.GUI();
+const debugObject = {};
+
+// make createShphere button !
+debugObject.createSphere = () => {
+  createSphere(Math.random() * 0.5, {
+    x: (Math.random() - 0.5) * 3,
+    y: 3,
+    z: (Math.random() - 0.5) * 3,
+  });
+};
+
+debugObject.createBox = () => {
+  // width, height, depth
+  createBox(Math.random(), Math.random(), Math.random(), {
+    x: (Math.random() - 0.5) * 3,
+    y: 3,
+    z: (Math.random() - 0.5) * 3,
+  });
+};
+
+debugObject.reset = () => {
+  for (const object of objectsToUpdate) {
+    // Remove body
+    object.body.removeEventListener("collide", playHitSound);
+    world.removeBody(object.body);
+
+    // Remove mesh
+    scene.remove(object.mesh);
+
+    // empty objectsToUpdate
+    objectsToUpdate.splice(0, objectsToUpdate.length);
+  }
+};
+
+gui.add(debugObject, "createSphere");
+gui.add(debugObject, "createBox");
+gui.add(debugObject, "reset");
 
 /**
  * Base
@@ -18,6 +55,24 @@ const canvas = document.querySelector("canvas.webgl");
 
 // Scene
 const scene = new THREE.Scene();
+
+/**
+ * Sounds
+ */
+const hitSound = new Audio("/sounds/hit.mp3");
+
+const playHitSound = (collision) => {
+  // check impact velocity. without it, if there is a small collision, it keeps making sound.
+  const impactStrength = collision.contact.getImpactVelocityAlongNormal();
+
+  if (impactStrength > 1.5) {
+    // make the sound natural
+    hitSound.volume = Math.random();
+    // prevent sound  from playing before collision
+    hitSound.currentTime = 0;
+    hitSound.play();
+  }
+};
 
 /**
  * Textures
@@ -37,21 +92,40 @@ const environmentMapTexture = cubeTextureLoader.load([
 /**
  * Physics
  */
+// World
 const world = new CANNON.World();
+
+// collision test optimization!1
+// When testing the collisions between objects, a naive approach is testing every Body against every other Body.
+// While this is easy to do, it's costly in terms of performance.
+// The broadphase is doing a rough sorting of the Bodies before testing them.
+world.broadphase = new CANNON.SAPBroadphase(world);
+// sleep optimization
+// When the Body speed gets incredibly slow (at a point where you can't see it moving),
+// the Body can fall asleep and won't be tested unless a sufficient force is applied to it by code or if another Body hits it.
+world.allowSleep = true;
+// apply gravity!
 world.gravity.set(0, -9.82, 0);
 
-// Sphere
-const sphereShape = new CANNON.Sphere(0.5);
-const sphereBody = new CANNON.Body({
-  mass: 1,
-  position: new CANNON.Vec3(0, 3, 0),
-  shape: sphereShape,
-});
-world.addBody(sphereBody);
+// Materials - set frictiion and bouncing
+const defaultMaterial = new CANNON.Material("concrete");
+// const plasticMaterial = new CANNON.Material("plastic");
+
+const defaultContactMaterial = new CANNON.ContactMaterial(
+  defaultMaterial,
+  defaultMaterial,
+  {
+    friction: 0.1,
+    restitution: 0.7, // return, restoration
+  }
+);
+world.addContactMaterial(defaultContactMaterial); // remind!
+world.defaultContactMaterial = defaultContactMaterial;
 
 // Floor - make static floor!
 const floorShape = new CANNON.Plane();
 const floorBody = new CANNON.Body();
+floorBody.material = defaultMaterial;
 floorBody.mass = 0;
 floorBody.addShape(floorShape);
 
@@ -62,22 +136,6 @@ floorBody.addShape(floorShape);
 floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5);
 
 world.addBody(floorBody);
-
-/**
- * Test sphere
- */
-const sphere = new THREE.Mesh(
-  new THREE.SphereGeometry(0.5, 32, 32),
-  new THREE.MeshStandardMaterial({
-    metalness: 0.3,
-    roughness: 0.4,
-    envMap: environmentMapTexture,
-    envMapIntensity: 0.5,
-  })
-);
-sphere.castShadow = true;
-sphere.position.y = 0.5;
-scene.add(sphere);
 
 /**
  * Floor
@@ -165,6 +223,85 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
+ * Utils
+ */
+const objectsToUpdate = [];
+
+// Sphere
+// optimize! - use scale
+const sphereGeometry = new THREE.SphereGeometry(1, 20, 20);
+const sphereMaterial = new THREE.MeshStandardMaterial({
+  metalness: 0.3,
+  roughness: 0.4,
+  envMap: environmentMapTexture,
+});
+
+const createSphere = (radius, position) => {
+  // Three.js mesh
+  const mesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  mesh.scale.set(radius, radius, radius);
+  mesh.castShadow = true;
+  mesh.position.copy(position);
+  scene.add(mesh);
+
+  // Cannon.js body
+  const shape = new CANNON.Sphere(radius);
+  const body = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 3, 0),
+    shape,
+    material: defaultMaterial,
+  });
+  body.position.copy(position);
+  body.addEventListener("collide", playHitSound);
+  world.addBody(body);
+
+  // Save in objects to update
+  objectsToUpdate.push({
+    mesh,
+    body,
+  });
+};
+
+// Box
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshStandardMaterial({
+  metalness: 0.3,
+  roughness: 0.4,
+  envMap: environmentMapTexture,
+});
+
+const createBox = (width, height, depth, position) => {
+  // Three.js mesh
+  const mesh = new THREE.Mesh(boxGeometry, boxMaterial);
+  mesh.scale.set(width, height, depth);
+  mesh.castShadow = true;
+  mesh.position.copy(position);
+  scene.add(mesh);
+
+  // Cannon.js body - the length should be half because it starts from center
+  const shape = new CANNON.Box(
+    new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5)
+  );
+  const body = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 3, 0),
+    shape,
+    material: defaultMaterial,
+  });
+  body.position.copy(position);
+  body.addEventListener("collide", playHitSound);
+  world.addBody(body);
+
+  // Save in objects to update
+  objectsToUpdate.push({
+    mesh,
+    body,
+  });
+};
+
+/**
  * Animate
  */
 const clock = new THREE.Clock();
@@ -178,8 +315,11 @@ const tick = () => {
   //Update physics world
   world.step(1 / 60, deltaTime, 3);
 
-  // you can copy all the properties as one with the copy(...) method:
-  sphere.position.copy(sphereBody.position);
+  for (const object of objectsToUpdate) {
+    object.mesh.position.copy(object.body.position);
+    // quaternion makes the movement of object natural in case of collisions and frictions. changes of angles.
+    object.mesh.quaternion.copy(object.body.quaternion);
+  }
 
   // Update controls
   controls.update();
