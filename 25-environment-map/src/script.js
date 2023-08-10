9eceb62064999dcd90d909e5f459c68757060451
Jay Kim <mearak43@gmail.com>
2023-08-10 19:45:05 +0900

    25 in progress 20:30

---
+++ 25-environment-map/src/script.js
@@ -0,0 +1,145 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as dat from "lil-gui";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
+/**
+ * Loaders
+ */
+
+const gltfLoader = new GLTFLoader();
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI();
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+// Update all materials
+const updateAllMaterials = () => {
+  scene.traverse((child) => {
+    if (child.isMesh && child.material.isMeshStandardMaterial) {
+      console.log("child", child);
+    }
+  });
+};
+
+/**
+ * Environment map
+ */
+// LDR cube texture
+const environmentMap = cubeTextureLoader.load([
+  "/environmentMaps/2/px.png",
+  "/environmentMaps/2/nx.png",
+  "/environmentMaps/2/py.png",
+  "/environmentMaps/2/ny.png",
+  "/environmentMaps/2/pz.png",
+  "/environmentMaps/2/nz.png",
+]);
+
+scene.environment = environmentMap;
+scene.background = environmentMap;
+
+/**
+ * Torus Knot
+ */
+const torusKnot = new THREE.Mesh(
+  new THREE.TorusKnotGeometry(1, 0.4, 100, 16),
+  new THREE.MeshStandardMaterial({
+    roughness: 0.3,
+    metalness: 1,
+    color: 0xaaaaaa,
+  })
+);
+torusKnot.position.y = 4;
+torusKnot.position.x = -4;
+scene.add(torusKnot);
+
+/**
+ * Models
+ */
+gltfLoader.load("models/FlightHelmet/glTF/FlightHelmet.gltf", (gltf) => {
+  console.log(gltf);
+  /**
+   * MeshStandardMaterial needs lights
+   */
+  gltf.scene.scale.set(10, 10, 10);
+  scene.add(gltf.scene);
+  updateAllMaterials();
+});
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.set(4, 5, 4);
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.target.y = 3.5;
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+const tick = () => {
+  // Time
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
