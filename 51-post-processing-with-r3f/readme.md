cab1b3b82af69546a11cddfd9e770ae0f22f16f6
Yongjae Kim <mearak43@gmail.com>
2023-10-08 14:10:35 +0900

    completed by performances, 51.post-processing

---
+++ 51-post-processing-with-r3f/readme.md
@@ -0,0 +1,42 @@
+`npm install @react-three/postprocessing@2.14`
+
+`npm install postprocessing@6.31`
+
+Post processing introduces the concept of passes and effects
+to extend the common rendering workflow with fullscreen image manipulation tools.
+
+multi-sampling is used to prevent the aliasing effect (the little stairs on the edges of geometries).
+
+## Post Processing:
+
+The repository: https://github.com/pmndrs/postprocessing
+The documentation (generated from the code): https://pmndrs.github.io/postprocessing/public/docs/
+A demo page (you can change the effect within the debug UI): https://pmndrs.github.io/postprocessing/public/demo/
+
+## React-postprocessing:
+
+The repository: https://github.com/pmndrs/reactpostprocessing
+The list of effects implemented from postprocessing to React postprocessing: https://github.com/pmndrs/postprocessing#included-effects
+The documentation (very similar to the repo, but more user-friendly): https://docs.pmnd.rs/react-postprocessing/introduction
+
+## Blending
+
+There is a special attribute named blendFunction available within Vignette but also with every other effects to come.
+
+blendFunction works a bit like the blending you can find in image editing softwares (like Photoshop). It’s how the color of what we are drawing merges with what’s behind.
+
+## Bloom Effect
+
+the default configuration of Bloom makes objects glow only when their color channels go beyond the 1 threshold.
+
+If you remember from the previous lessons, a tone mapping is applied by default. While it makes the colors look great, the tone mapping will clamp the colors between 0 and 1.
+
+## DepthOfField effect
+
+This effect will blur what’s closer or further from a set distance.
+
+## SSR Effect
+
+React Three Fiber Examples: https://docs.pmnd.rs/react-three-fiber/getting-started/examples
+
+Code Ref: https://codesandbox.io/s/ssr-test-8pbw1f?file=/src/App.js:0-31
---
+++ 51-post-processing-with-r3f/src/Experience.jsx
@@ -0,0 +1,106 @@
+import { OrbitControls } from "@react-three/drei";
+import {
+  Bloom,
+  DepthOfField,
+  EffectComposer,
+  Glitch,
+  Noise,
+  SSR,
+  Vignette,
+} from "@react-three/postprocessing";
+import { useControls } from "leva";
+import { BlendFunction, GlitchMode } from "postprocessing";
+import { Perf } from "r3f-perf";
+
+export default function Experience() {
+  //   const ssrProps = useControls("SSR Effect", {
+  //     temporalResolve: true,
+  //     STRETCH_MISSED_RAYS: true,
+  //     USE_MRT: true,
+  //     USE_NORMALMAP: true,
+  //     USE_ROUGHNESSMAP: true,
+  //     ENABLE_JITTERING: true,
+  //     ENABLE_BLUR: true,
+  //     temporalResolveMix: { value: 0.9, min: 0, max: 1 },
+  //     temporalResolveCorrectionMix: { value: 0.25, min: 0, max: 1 },
+  //     maxSamples: { value: 0, min: 0, max: 1 },
+  //     resolutionScale: { value: 1, min: 0, max: 1 },
+  //     blurMix: { value: 0.5, min: 0, max: 1 },
+  //     blurKernelSize: { value: 8, min: 0, max: 8 },
+  //     blurSharpness: { value: 0.5, min: 0, max: 1 },
+  //     rayStep: { value: 0.3, min: 0, max: 1 },
+  //     intensity: { value: 1, min: 0, max: 5 },
+  //     maxRoughness: { value: 0.1, min: 0, max: 1 },
+  //     jitter: { value: 0.7, min: 0, max: 5 },
+  //     jitterSpread: { value: 0.45, min: 0, max: 1 },
+  //     jitterRough: { value: 0.1, min: 0, max: 1 },
+  //     roughnessFadeOut: { value: 1, min: 0, max: 1 },
+  //     rayFadeOut: { value: 0, min: 0, max: 1 },
+  //     MAX_STEPS: { value: 20, min: 0, max: 20 },
+  //     NUM_BINARY_SEARCH_STEPS: { value: 5, min: 0, max: 10 },
+  //     maxDepthDifference: { value: 3, min: 0, max: 10 },
+  //     maxDepth: { value: 1, min: 0, max: 1 },
+  //     thickness: { value: 10, min: 0, max: 10 },
+  //     ior: { value: 1.45, min: 0, max: 2 },
+  //   });
+
+  return (
+    <>
+      <color args={["#ffffff"]} attach="background" />
+
+      <EffectComposer multisampling={0}>
+        {/* <Vignette
+          offset={0.3}
+          darkness={0.9}
+          blendFunction={BlendFunction.NORMAL}
+        /> */}
+        {/* <Glitch
+          delay={[0.5, 1]}
+          duration={[0.1, 0.3]}
+          strength={[0.2, 0.4]}
+          mode={GlitchMode.CONSTANT_WILD}
+        /> */}
+        {/* <Noise premultiply blendFunction={BlendFunction.AVERAGE} /> */}
+        {/* <Bloom mipmapBlur intensity={0.5} luminanceThreshold={0} /> */}
+        {/* <DepthOfField
+          focusDistance={0.025}
+          focalLength={0.025}
+          bokehScale={6}
+        /> */}
+        {/* <SSR {...ssrProps} /> */}
+      </EffectComposer>
+
+      <Perf position="top-left" />
+      <OrbitControls makeDefault />
+      <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
+      <mesh castShadow position-x={-2}>
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+      <mesh castShadow position-x={2} scale={1.5}>
+        <boxGeometry />
+        {/* this gets light to make bloom  */}
+        {/* <meshStandardMaterial
+          // color={[1.5, 1, 4]}
+          color={"white"}
+          emissive={"orange"}
+          emissiveIntensity={1}
+          toneMapped={false}
+        /> */}
+        {/* an uniform emissive. blooms to all of directions */}
+        {/* <meshBasicMaterial color={[1.5, 1, 4]} toneMapped={false} /> */}
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+      <mesh
+        receiveShadow
+        position-y={-1}
+        rotation-x={-Math.PI * 0.5}
+        scale={10}
+      >
+        <planeGeometry />
+        <meshStandardMaterial color="#000000" metalness={0} roughness={0} />
+      </mesh>
+    </>
+  );
+}
